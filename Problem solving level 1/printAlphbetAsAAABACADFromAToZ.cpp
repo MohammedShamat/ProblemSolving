9ab5fd90c159f19c3b33b07f776ac2e92dde3b50
Mohammed <mohammedshamat.dev@gmail.com>
2023-06-30 11:30:27 +0300

    print Alphbet From A To Z As AA AB AC...

---
+++ Problem solving level 1/printAlphbetAsAAABACADFromAToZ.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+	for (char alphbet = 65; alphbet <= 90;alphbet++) {
+		for (char secondeAlphbet = 65; secondeAlphbet <= 90;secondeAlphbet++) {
+			cout << alphbet << secondeAlphbet << endl;
+		}
+	}
+
+	/*out put 
+	AA
+	AB
+	AC
+	.
+	.
+	.
+	ZZ
+	*/
+
+}
