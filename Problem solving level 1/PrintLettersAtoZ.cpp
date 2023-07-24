dfa27d298fa46709c135047b3556f310b5843518
MohammedShamat <mohammedshamat.dev@gmail.com>
2023-07-24 03:41:15 +0300

    PrintLettersAtoZ

---
+++ Problem solving level 1/PrintLettersAtoZ.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void PrintLettersAtoZ() {
+	for (char alphbet = 65; alphbet <= 90; alphbet++) {
+		cout << alphbet << endl;
+	}
+}
+
+int main()
+{
+	PrintLettersAtoZ();
+}/*
+ A
+B
+C
+D
+E
+F
+G
+H
+I
+J
+K
+L
+M
+N
+O
+P
+Q
+R
+S
+T
+U
+V
+W
+X
+Y
+Z*/
