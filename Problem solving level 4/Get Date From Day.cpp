72563b598318a70446ad18a54360ff97757358d1
MohammedShamat <mohammedshamat.dev@gmail.com>
2023-11-05 21:46:59 +0300

    Get Date From Day

---
+++ Problem solving level 4/Get Date From Day.cpp
@@ -0,0 +1,117 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<iomanip>
+#include<cstdio>
+using  namespace std;
+
+struct Date {
+	short year;
+	short month;
+	short day;
+};
+
+short ReadYear(string message) {
+	int number = 0;
+	cout << message << endl;
+	cin >> number;
+	while (cin.fail()) {
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "Invalid Number, Enter a valid one\n";
+		cin >> number;
+	}
+	return number;
+}
+
+short ReadMonth(string message) {
+	int number = 0;
+	cout << message << endl;
+	cin >> number;
+	while (cin.fail() || (number < 1 || number>12)) {
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "Invalid Number, Enter a valid one\n";
+		cin >> number;
+	}
+	return number;
+}
+
+short ReadDay(string message) {
+	int number = 0;
+	cout << message << endl;
+	cin >> number;
+	while (cin.fail() || (number < 1 || number>31)) {
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "Invalid Number, Enter a valid one\n";
+		cin >> number;
+	}
+	return number;
+}
+
+bool IsLeapYear(short year) {
+	return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+}
+
+short DaysInMonth(short year, short month) {
+
+	short arr31Dyas[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	return  (month == 2) ? (IsLeapYear(year) ? 29 : 28) : arr31Dyas[month - 1];
+
+}
+
+short TotalDaysFromBeginningYear(short year, short month, short days) {
+	short totalDays = 0;
+	for (short counter = 1; counter <= month - 1; counter++) {
+		totalDays += DaysInMonth(year, counter);
+	}
+
+	return (totalDays + days);
+
+}
+
+Date GetDateFromDay(short year, short days) {
+	Date date;
+	short RemainingDays = days;
+	short monthDays = 0;
+	date.year = year;
+	date.month = 1;
+	while (true) {
+		monthDays = DaysInMonth(year, date.month);
+		if (RemainingDays > monthDays) {
+			RemainingDays -= monthDays;
+			date.month++;
+		}
+		else {
+			date.day = RemainingDays;
+			break;
+		}
+	}
+	return date;
+}
+
+int main() {
+	short yearFromUser = ReadYear("Enter a year?");
+	short monthFromUser = ReadMonth("Please Enter a month");
+	short dayFromUser = ReadDay("Please Enter A Day");
+	short daysTotal = TotalDaysFromBeginningYear(yearFromUser, monthFromUser, dayFromUser);
+	Date ConvertToDate = GetDateFromDay(yearFromUser, daysTotal);
+	cout << "\nNumber of days from the beginning of the year is " << daysTotal << endl;
+	cout << "Date for [" << daysTotal << "] is :" << ConvertToDate.day << "/" << ConvertToDate.month << "/" << ConvertToDate.year;
+
+}
+
+
+/*
+output
+Enter a year?
+2022
+Please Enter a month
+9
+Please Enter A Day
+20
+
+Number of days from the beginning of the year is 263
+Date for [263] is :20/9/2022
+*/
